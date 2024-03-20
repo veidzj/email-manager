d56f16ee5a430500fe0ca77ac7a2fe77738308a3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-20 16:22:47 -0300

    feat: add save_template

--- email_manager.py
+++ email_manager.py
@@ -1,33 +1,44 @@
+from pathlib import Path
 import streamlit as st
 
+current_folder = Path(__file__).parent
+templates_foler = current_folder / 'templates'
 
 if not 'email_manager_page' in st.session_state:
   st.session_state.email_manager_page = 'home'
 
-def change_page(page_name: str) -> None:
+def change_page(page_name: str):
   st.session_state.email_manager_page = page_name
 
-def home_page() -> None:
+def home_page():
   st.markdown('# Email Manager')
 
-def templates_page() -> None:
+def templates_page():
   st.markdown('# Templates')
 
   st.button('Add template', on_click=change_page, args=('add_template',))
 
-def add_template_page() -> None:
+def add_template_page():
   template_name = st.text_input('Template name:')
   template_text = st.text_area('Write the template text:', height=600)
+  st.button('Save', on_click=save_template, args=(template_name, template_text))
 
-def email_list_page() -> None:
+def save_template(name, text):
+  templates_foler.mkdir(exist_ok=True)
+  file_name = name.replace(' ', '_').lower() + '.txt'
+  with open(templates_foler / file_name, 'w', encoding='utf-8') as file:
+    file.write(text)
+  change_page('templates')
+
+def email_list_page():
   st.markdown('# Email List')
 
-def settings_page() -> None:
+def settings_page():
   st.markdown('# Settings')
 
 
 
-def main() -> None:
+def main():
   st.sidebar.button('Email Manager', use_container_width=True, on_click=change_page, args=('home',))
   st.sidebar.button('Templates', use_container_width=True, on_click=change_page, args=('templates',))
   st.sidebar.button('Email List', use_container_width=True, on_click=change_page, args=('email_list',))
